5b61f5230f92805ed33017671ddb6e45d62800ec
132rahul <30731690+132rahul@users.noreply.github.com>
2017-08-26 18:56:05 +0530

    Update SinglePattern.java

--- SinglePattern.java
+++ SinglePattern.java
@@ -1,12 +1,25 @@
-class SinglePattern{  
-	 private static SinglePattern instance;
-	    
-	    private SinglePattern(){}
-	    
-	    public static synchronized SinglePattern getInstance(){
-	        if(instance == null){
-	            instance = new SinglePattern();
-	        }
-	        return instance;
-	    }  
+class singleton {
+	private static singleton obj = null;
+
+	public static singleton getobj() {
+
+		if (obj == null) {
+			synchronized (obj) {
+				if (obj == null) {
+					obj = new singleton();
+				}
+			}
+		}
+		return obj;
+	}
+}
+
+class SinglePattern {
+	public static void mian(String[] args) {
+		singleton ob = new singleton();
+		singleton ob1 = new singleton();
+		singleton ob2 = new singleton();
+		singleton ob3 = new singleton();
+
+	}
 }
